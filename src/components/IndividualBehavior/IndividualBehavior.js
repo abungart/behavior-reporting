8a02ab56640333916148c5f3387839f916e19ff6
Adam Bungart <abungart@gmail.com>
2020-05-13 14:49:30 -0500

    Added hourly intervention form

--- src/components/IndividualBehavior/IndividualBehavior.js
+++ src/components/IndividualBehavior/IndividualBehavior.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import StudentData from "../StudentData/StudentData";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const hour = [
+  { value: "8am to 9am", label: "8" },
+  { value: "9am to 10am", label: "9" },
+  { value: "10am to 11am", label: "10" },
+  { value: "11am to 12pm", label: "11" },
+  { value: "12pm to 1pm", label: "12" },
+  { value: "1pm to 2pm", label: "1" },
+  { value: "2pm to 3pm", label: "2" },
+  { value: "3pm to 4pm", label: "3" },
+];
 
 class IndividualBehavior extends Component {
   componentDidMount() {
@@ -11,6 +25,9 @@ class IndividualBehavior extends Component {
   state = {
     heading: "Individual Behavior Page!",
     pointGoal: 0,
+    notes: "",
+    date: new Date(),
+    hour: "",
   };
 
   interventionSwitch = () => {
@@ -33,6 +50,10 @@ class IndividualBehavior extends Component {
     });
   }; // end InterventionSwitch
 
+  submitHourlyIntervention = () => {
+    console.log("In Hourly Submit", this.state);
+  };
+
   // Text Input Handler
   handlePointsChange = (event) => {
     this.setState({
@@ -40,7 +61,26 @@ class IndividualBehavior extends Component {
     });
   };
 
+  handleNotesChange = (event) => {
+    this.setState({
+      notes: event.target.value,
+    });
+  };
+
+  handleDateChange = (date) => {
+    this.setState({
+      date: date,
+    });
+  };
+
+  handleHourChange = (selectedOption) => {
+    this.setState({
+      hour: selectedOption.value,
+    });
+  };
+
   render() {
+    const { selectedOption } = this.state.hour;
     return (
       <div>
         <h2>"{this.state.heading}"</h2>
@@ -61,9 +101,39 @@ class IndividualBehavior extends Component {
             </div>
           )}
           {this.props.store.studentInfo.in_intervention === true && (
-            <button type="button" onClick={this.interventionSwitch}>
-              Stop Intervention
-            </button>
+            <div>
+              <div>
+                <button type="button" onClick={this.interventionSwitch}>
+                  Stop Intervention
+                </button>
+              </div>
+              <div>
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.handleDateChange}
+                />
+                <Select
+                  value={selectedOption}
+                  onChange={this.handleHourChange}
+                  options={hour}
+                />
+                <label>Points this hour:</label>
+                <input
+                  type="number"
+                  placeholder="0"
+                  onChange={this.handlePointsChange}
+                />
+                <label>Notes for the hour:</label>
+                <input
+                  type="textBlock"
+                  placeholder="Notes"
+                  onChange={this.handleNotesChange}
+                />
+                <button type="button" onClick={this.submitHourlyIntervention}>
+                  Submit Hourly Intervention
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </div>
--- src/components/InterventionStart/InterventionStart.js
+++ src/components/InterventionStart/InterventionStart.js
@@ -4,13 +4,23 @@ import { withRouter } from "react-router-dom";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 
 class InterventionStart extends Component {
+  studentBehavior = () => {
+    console.log("In studentBehavior Click", this.props.userItem);
+    this.props.dispatch({
+      type: "FETCH_STUDENT_SEPARATE",
+      payload: this.props.userItem,
+    });
+
+    this.props.history.push("/individualBehavior");
+  };
+
   render() {
     console.log("User Item", this.props.userItem);
     return (
       <div>
         {this.props.userItem.in_intervention === true && (
           <li className="user_list">
-            <h2>{this.props.userItem.name}</h2>
+            <h2 onClick={this.studentBehavior}>{this.props.userItem.name}</h2>
           </li>
         )}
       </div>
