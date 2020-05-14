b70bd9cfbdeaac31ae82c0aec7df14e9cc3f587a
Adam Bungart <abungart@gmail.com>
2020-05-14 11:39:25 -0500

    All Sagas for GET intervention routes ready

--- src/redux/sagas/intervention.saga.js
+++ src/redux/sagas/intervention.saga.js
@@ -45,11 +45,50 @@ function* getDailyIntervention(action) {
   }
 }
 
+// GET all of one student's interventions
+function* getStudentInterventions(action) {
+  try {
+    const thisStudent = action.payload;
+    const response = yield axios.get(
+      `/api/intervention/studentInterventions/${thisStudent.id}`
+    );
+    yield put({ type: "SET_PERIOD_INTERVENTION", payload: response.data });
+  } catch (err) {
+    console.log("Error with getStudentInterventions", err);
+  }
+}
+
+// GET all of one teacher's interventions
+function* getTeacherInterventions(action) {
+  try {
+    const teacher = action.payload;
+    const response = yield axios.get(
+      `/api/intervention/teacherInterventions/${teacher.id}`
+    );
+    yield put({ type: "SET_PERIOD_INTERVENTION", payload: response.data });
+  } catch (err) {
+    console.log("Error with getTeacherInterventions", err);
+  }
+}
+
+// GET all of school interventions
+function* getSchoolInterventions(action) {
+  try {
+    const response = yield axios.get("/api/intervention/schoolInterventions");
+    yield put({ type: "SET_PERIOD_INTERVENTION", payload: response.data });
+  } catch (err) {
+    console.log("Error with getSchoolInterventions", err);
+  }
+}
+
 function* userListSaga() {
   yield takeLatest("INTERVENTION_TOGGLE", interventionToggle);
   yield takeLatest("START_DAILY_INTERVENTION", startDailyIntervention);
   yield takeLatest("SUBMIT_HOURLY_INTERVENTION", startHourlyIntervention);
   yield takeLatest("DAILY_INTERVENTION", getDailyIntervention);
+  yield takeLatest("STUDENT_PERIOD_INTERVENTION", getStudentInterventions);
+  yield takeLatest("TEACHER_INTERVENTION", getTeacherInterventions);
+  yield takeLatest("SCHOOL_INTERVENTION", getSchoolInterventions);
 }
 
 export default userListSaga;
